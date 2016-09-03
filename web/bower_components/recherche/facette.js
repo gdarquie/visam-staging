259977de14a245c6e070caf58e799a7133d721f6
taclab <mehdi.bourgeois@gmail.Com>
2016-09-03 17:36:03 +0200

    Intégration technique de la recherche : done!

--- web/bower_components/recherche/facette.js
+++ web/bower_components/recherche/facette.js
@@ -2,21 +2,21 @@
           var visam_temlate = 
            '<% if (obj.type == "Formation") {  %><div class="card formation">' +
             '<div class="card-content">'+
-            '<span class="item-labo">Formation</span>'+
-            '<span class="etablissement"><span>- <%= obj.ctype %> </span><%= obj.code %></span>'+
-             '<a href="/Formation/<%= obj.id %>">'+
-              '<h5><%= obj.name %> (<%= obj.sigle %>)</h5></a>'+
+            '<span class="item-formation">Formation</span>'+
+            '<span class="etablissement"><span>- <%= obj.niveau %> </span><%= obj.typeDiplome %></span>'+
+             '<a href="/formation/<%= obj.id %>">'+
+              '<h5><%= obj.name %></h5></a>'+
               '<span><%= obj.etablissement %></span>'+
               '<ul class="list-thematique" >' + 
               '<li><%= obj.hesamette %></li>'+
-              '<ul>'+
+              '</ul>'+
+              '</div>'+
               '<div class="card-action">'+
-              '<a href="<%= obj.url %>"><i class="material-icons">input</i>Lien vers le laboratoire</a>'+
+              '<a href="<%= obj.url %>"><i class="material-icons">input</i>Lien vers la formation</a>'+
                 '<div style="float:right;">'+
-                '<span><a href="mailto:<%= obj.mailcontact %>"><i class="material-icons">mail</i><%= obj.mailcontact %></a></span>'+
+                '<span>Effectif(s): <%= obj.effectif %> - <%= obj.annee %></span>'+
                 '</div>'+
               '</div>'+
-            '</div>'+
            '</div><% } %>'+
           '<% if (obj.type == "Laboratoire") {  %><div class="card laboratoire">' +
             '<div class="card-content">'+
@@ -28,34 +28,35 @@
               '<ul class="list-thematique" >' + 
               '<li><%= obj.hesamette %></li>'+
               '<ul>'+
+              '</div>'+
               '<div class="card-action">'+
               '<a href="<%= obj.url %>"><i class="material-icons">input</i>Lien vers le laboratoire</a>'+
                 '<div style="float:right;">'+
                 '<span><a href="mailto:<%= obj.mailcontact %>"><i class="material-icons">mail</i><%= obj.mailcontact %></a></span>'+
                 '</div>'+
-              '</div>'+
             '</div>'+
-           '</div><% } %>'
-           ;
+           '</div><% } %>';
 
 
 
           settings = { 
             items            : dataJson,
             facets           : { 
-                                'type' : 'type',
-                                'etablissement'     : 'etablissement',
-                                'hesamette'     : 'hesamette',
+                                'type' : 'Type',
+                                'etablissement'     : 'Etablissements',
+                                'hesamette'     : 'Disciplines',
             },  
             resultSelector   : '#results',
             facetSelector    : '#facets',
+            facetTitleTemplate : '<h4 class=facettitle><%= title %></h4>',
             resultTemplate   : visam_temlate,
-
-            orderByTemplate    : '<div class=orderby><span class="orderby-title">Sort by: </span><ul><% _.each(options, function(value, key) { %>'+
+            countTemplate      : '<div class=facettotalcount><%= count %> Résultats</div>',
+            deselectTemplate   : '<div class=deselectstartover>Suppression des filtres</div>',
+            orderByTemplate    : '<div class=orderby><span class="orderby-title">- Trier par : </span><ul><% _.each(options, function(value, key) { %>'+
                        '<li class=orderbyitem id=orderby_<%= key %>>'+
                        '<%= value %> </li> <% }); %></ul></div>',
-            orderByOptions     : {'a': 'Par A', 'b': 'Par B', 'RANDOM': 'Par Hasard'},
-
+            orderByOptions     : {'a': 'Par A', 'b': 'Par B'},
+            noResults          : '<div class=results>Désolé, nous trouvons aucun résultat ! </div>',
             paginationCount  : 50,
             enablePagination   : true,
           } 
@@ -64,17 +65,36 @@
         // use them!
         $.facetelize(settings);
 
-        $("#search-input").change(function () {
+
+
+        $('#search-input').keyup(function () { 
           var searchVal = $("#search-input").val();
+          if (searchVal) {
+            var returnedData = $.grep(dataJson, function(element, index){
+              if (element.name.indexOf(searchVal) >= 0) {
+                return element.name.indexOf(searchVal);
+              }
+            });
+            settings.items = returnedData;
+          } else {
+            settings.items = dataJson;
+          }
 
-          var returnedData = $.grep(dataJson, function(element, index){
-            if (element.name.indexOf(searchVal) >= 0) {
-              return element.name.indexOf(searchVal);
-            }
-          });
-
-          settings.items = returnedData;
           $.facetelize(settings);
+
         });
+
+        // $("#search-input").change(function () {
+        //   var searchVal = $("#search-input").val();
+
+        //   var returnedData = $.grep(dataJson, function(element, index){
+        //     if (element.name.indexOf(searchVal) >= 0) {
+        //       return element.name.indexOf(searchVal);
+        //     }
+        //   });
+
+        //   settings.items = returnedData;
+        //   $.facetelize(settings);
+        // });
         
       });
