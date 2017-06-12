72aeb2f4e109c912e40f35760e50bb0768cc5000
gaetan <gaetan.darquie@gmail.com>
2017-06-13 00:04:58 +0200

    merge sandra

---
+++ app/DoctrineMigrations/Version20170612220207.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170612220207 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE formation ADD code_interne VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE labo ADD code_interne VARCHAR(100) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE formation DROP code_interne');
+        $this->addSql('ALTER TABLE labo DROP code_interne');
+    }
+}
